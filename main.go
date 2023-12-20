fcb26323e536bb1e1fb8c718f7dafd9b81c5b848
Wang Zhigao <aimiskamei@gmail.com>
2023-12-20 21:50:54 +0800

    Routing (using gorilla/mux)

--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Welcome to my web site!")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
 	})
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	// TCP connection，when handler param is nil use DefaultServeMux
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":80", r)
 }
